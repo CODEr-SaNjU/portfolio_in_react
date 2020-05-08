90bd8947c506e746d99ac5c90198c94d04b37172
CoDEr-SaNjU <sanju2help@gmail.com>
2020-05-08 09:46:43 +0530

    update_portfolio

---
+++ portfolio_sanju/src/components/sidebar.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+              <div className="author-img" style={{backgroundImage: 'url(images/img_bg.jpg)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">SaNjU Saini</a></h1>
+              <span class="position"><a href="#">Python Developer</a> in Bangalore(india)</span>
+              <span className="email"><i className="icon-mail"></i> SaNjU2help@gmail.com</span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li><a href="#about" data-nav-section="about">About</a></li>
+                  <li><a href="#project" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#blog" data-nav-section="blog">Blog</a></li>
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                  {/* <li><a href="#contact" data-nav-section="contact">Contact</a></li> */}
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+                <li><a href="#" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://twitter.com/SWwET_FoRvEr" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+                <li><a href="https://www.instagram.com/_mr_tdb/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                <li><a href="https://www.linkedin.com/in/sanju-saini-246580154" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/CODEr-SaNjU" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="https://medium.com/@sanju2help" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+            <p><small>&copy;Copyright &copy; 2020 All rights reserved</small></p>
+              <p><small>
+                  Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
+                  Thanks <a href="#" target="_blank" rel="noopener noreferrer">Ma <i className="icon-heart" aria-hidden="true" /> </a> for inspiration 
+              </small></p>
+              <p><small>
+                Something coming soon !!
+              </small></p>
+            </div>
+          </aside>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ portfolio_sanju/src/components/timeline.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-6">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Junior Software Developer at Indian Structurals & Conveyors Pvt. Ltd <span>Feb 2020 - Present.</span></h2>
+                        <p>i recently have join Indian Structurals & Conveyors Pvt. Ltd as a software developer</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Fullstack trainee at BYTE ACADEMY <span>Sep.2019-Jan.2020</span></h2>
+                        <p>I recently have done Python Full Stack Developer Course in the Byte Academy i learned to many technologies like Python javascript Django Flask React ....! </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Diploma at RPGPC<span> 2017-2019</span></h2>
+                        <p>I have done my Diploma studies with major in CSE(computer science and engineering). I have taken courses like DSA, OOPs, Computer Networks, Web-Data Management over the years and have better understanding of these subjects.I have also been part of III CELL(The industry-institute interaction cell) scince my first year of college.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 76%. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
